0ce03de19e978631bbc718d7e1ca0024d00dc03c
Vlad Barishnikov <barushnikovvs@gmail.com>
2024-10-11 22:59:57 +0400

    feat: добавил описание эндпоинтов в сваггере

--- DistrictEnterpriseStatisticalData.Api/Program.cs
+++ DistrictEnterpriseStatisticalData.Api/Program.cs
@@ -9,7 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opt =>
+{
+    var path = Path.Combine(AppContext.BaseDirectory, "DistrictEnterpriseStatisticalData.Api.xml");
+    opt.IncludeXmlComments(path);
+});
 
 builder.Services.AddAutoMapper(typeof(Mapper));
 
